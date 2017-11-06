89405eae52f73c7f1b590156c109c4043a977415
atpdev14 <tproctor4414@hotmail.com>
2017-11-06 15:00:40 -0800

    initial commit.  html content added with some basic styling.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// var config = {
+//     apiKey: "AIzaSyCvQrBaJ62p6smjlowyYUia08qvezEqVN4",
+//     authDomain: "rock-paper-scissors-a8d65.firebaseapp.com",
+//     databaseURL: "https://rock-paper-scissors-a8d65.firebaseio.com",
+//     projectId: "rock-paper-scissors-a8d65",
+//     storageBucket: "",
+//     messagingSenderId: "529638228111"
+//   };
+//   firebase.initializeApp(config);
