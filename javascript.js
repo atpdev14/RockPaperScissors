7934b9d4eb4461b503db8d859803923bbb63f7b0
atpdev14 <tproctor4414@hotmail.com>
2017-11-06 15:26:34 -0800

    Adding some game functionality

--- javascript.js
+++ javascript.js
@@ -7,3 +7,44 @@
 //     messagingSenderId: "529638228111"
 //   };
 //   firebase.initializeApp(config);
+
+var choices = ["Rock", "Paper", "Scissors"];
+var p1Selected = 0;
+var p1Selection;
+var p2Selection;
+
+for(i = 0; i < choices.length; i++){
+	var button = $("<button>");
+	button.text(choices[i]);
+	button.attr("value", choices[i]);
+	button.addClass("userSelection");
+	$("#player1").append(button);
+	$("#player2").append(button.clone());
+}
+
+
+$(".userSelection").on("click", function(){
+	if (p1Selected === 0) {
+		p1Selection = $(this).val();
+		p1Selected++;
+		console.log("P1: " + p1Selection);
+	} else if (p1Selected === 1) {
+		p2Selection = $(this).val();
+		console.log("P2: " + p2Selection);
+		p1Selected++;
+	} else {
+		alert("Both Players have already made a selection!");
+	}
+});
+
+
+
+
+
+
+
+
+// use the p1Selected variable to enable the
+// first player to select a value for Player 1
+// then update the boolean value within the database.
+// Then allow the second player to make a selection.
