a3416be8ab63d274bd6334689e558da41dcfcfb6
atpdev14 <tproctor4414@hotmail.com>
2017-11-09 09:46:32 -0800

    submittable

--- javascript.js
+++ javascript.js
@@ -15,6 +15,43 @@ var p1Selection;
 var p2Selection;
 var p1Wins = 0;
 var p2Wins = 0;
+var turnCounter = 0;
+
+function winConditions() {
+	$("#p1Wins").text(p1Selection);
+	$("#p2Wins").text(p2Selection);
+
+	if(p1Selection === "Rock" && p2Selection === "Rock"){
+		$("#gamePrompt").text("You tied!");
+	} else if (p1Selection === "Rock" && p2Selection === "Paper") {
+		$("#gamePrompt").text("Player 2 Wins");
+		p2Wins++;
+	} else if(p1Selection === "Rock" && p2Selection === "Scissors") {
+		$("#gamePrompt").text("Player 1 Wins");
+		p1Wins++;
+	} else if(p1Selection === "Paper" && p2Selection === "Paper") {
+		$("#gamePrompt").text("You tied!");
+	} else if(p1Selection === "Paper" && p2Selection === "Scissors") {
+		$("#gamePrompt").text("Player 2 Wins");
+		p2Wins++;
+	} else if(p1Selection === "Paper" && p2Selection === "Rock") {
+		$("#gamePrompt").text("Player 1 Wins");
+		p1Wins++;
+	} else if(p1Selection === "Scissors" && p2Selection === "Scissors") {
+		$("#gamePrompt").text("You tied!");
+	} else if(p1Selection === "Scissors" && p2Selection === "Rock") {
+		$("#gamePrompt").text("Player 2 Wins");
+		p2Wins++;
+	} else if(p1Selection === "Scissors" && p2Selection === "Paper") {
+		$("#gamePrompt").text("Player 1 Wins");
+		p1Wins++;
+	}
+}
+
+// Initialize p2Turn boolean in database to control turns
+database.ref("/p1").set({
+	p2Turn: p2Turn
+		});
 
 // ==================================
 // 		PRINT BUTTONS
@@ -34,6 +71,7 @@ $(".userSelection").on("click", function(){
 	if (p2Turn === false) {
 		p1Selection = $(this).val();
 		p2Turn = true;
+		turnCounter++;
 		$("#gamePrompt").text("Player 2- Make a Selection");
 		$("#player").text("Player Two");
 		
@@ -42,16 +80,17 @@ $(".userSelection").on("click", function(){
 			p1Selection: p1Selection,
 			p2Turn: p2Turn
 		});
+
 	} else if (p2Turn === true) {
 		p2Selection = $(this).val();
 		p2Turn = false;
+		turnCounter++;
 
 		database.ref("/p2").set({
 			p2Selection: p2Selection,
 			p2Turn: p2Turn
 		});
-	} else {
-		alert("Both Players have already made a selection!");
+		winConditions();
 	}
 });
 
@@ -60,36 +99,23 @@ $(".userSelection").on("click", function(){
 // 		VALUE CHANGE LISTENER
 // ==================================
 database.ref().on("value", function(snapshot) {
-	p1Selection = database.ref().p1Selection;
-	p2Selection = database.ref().p2Selection;
-
 	console.log("P1: " + snapshot.child("p1/p1Selection").val());
 	console.log("P2: " + snapshot.child("p2/p2Selection").val());
+
+	p1Selection = snapshot.child("p1/p1Selection").val();
+	p2Selection = snapshot.child("p2/p2Selection").val();
+	p2Turn = snapshot.child("p1/p2Turn").val();
+	console.log(typeof(p2Turn));
 
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
 
-// snapshot.child(‘players/two/name/’).val()
 
 
-
-
-
-
-
-
-// -Player one makes a selection and updates that 
-// variable value to the database
-// -Change booelan value of p2Turn in database
-// -Retrieve that value from the database
-// and set it equal to the local variable
-// -Retrieve boolean value and update it locally
-// -Use boolean value for player 2 to make a selection
-// -compare selections and determine winner
-// -Update win boolean to database
-// -retrieve win boolean
-// -use win boolean to display winner and update win values
+// -add a reset function to start the game over
+// 	-reset p2Turn boolean, html text
+// 	-add win counters to database and display them in html
 
 
 
